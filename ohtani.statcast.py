f9c0d0f4439d4e6ca258e0af20ac24d3a99ba12f
小若憲史 <kenjikowaka@kowakaknjinoMBP.lan>
2024-01-08 19:43:20 +0900

    first commit

---
+++ ohtani.statcast.py
@@ -0,0 +1,12 @@
+     pitch_type  game_date  release_speed  release_pos_x  ...  of_fielding_alignment spin_axis  delta_home_win_exp  delta_run_exp
+2021         FF 2023-08-23           94.2          -1.98  ...               Standard       204                 0.0            0.0
+2097         ST 2023-08-23           76.1          -2.01  ...               Standard        65                 0.0         -0.064
+2111         ST 2023-08-23           77.2          -2.05  ...               Standard        69                 0.0         -0.072
+2224         FS 2023-08-23           90.1          -1.94  ...               Standard       317                 0.0          0.063
+2267         FS 2023-08-23           90.0          -1.83  ...               Standard       191                 0.0          0.042
+...         ...        ...            ...            ...  ...                    ...       ...                 ...            ...
+3969         FF 2023-07-04           92.2           -2.2  ...               Standard       220                 0.0         -0.031
+4096         FC 2023-07-04           80.2          -2.22  ...               Standard       101                 0.0          0.025
+4214         FC 2023-07-04           87.0          -2.19  ...               Standard       183              -0.021         -0.228
+4308         ST 2023-07-04           79.5          -2.25  ...               Standard        78                 0.0         -0.046
+4469         FF 2023-07-04           92.6          -1.81  ...               Standard       213                 0.0          0.036
